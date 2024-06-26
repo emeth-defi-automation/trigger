3988ddbb80b436e37f9adba51a92dcf30c002c32
Gawc1uuu <gawlikjakub9@gmail.com>
2024-06-26 19:12:24 +0200

    mined tx + minimal_example

--- automation_trigger/src/main.rs
+++ automation_trigger/src/main.rs
@@ -1,75 +1,99 @@
 use ethers::{
+    types::U256,
     contract::abigen,
-    core::types::{Address, Filter},
+    core::types::{Address, Filter,TransactionRequest},
     providers::{Provider, Ws},
 };
 use ethers::prelude::*;
 use eyre::Result;
+use resolver::print;
 use serde_json::Number;
-use std::{sync::Arc};
-use tokio;
+use std::{sync::Arc, time::{SystemTime, UNIX_EPOCH}};
+use tokio::time::{sleep_until, Instant, Duration};
 use chrono::{Utc};
+use hex_literal::hex;
+use std::str;
+
+type Client = SignerMiddleware<Provider<Http>, 
+Wallet<k256::ecdsa::SigningKey>>;
 
 abigen!(
     MyContract,
     r#"[
-    event ActionExecutionAttempted(uint256 actionId, string message, uint256 timeZero, address contractAddress)
+    event ActionExecutionAttempted(uint256 actionId, uint256 timeZero, address contractAddress)
     function executeAction(uint256 actionId) public returns (bool)
     ]"#,
 );
 
+abigen!(
+    TransferAction,
+    r#"[
+        function getActionById(uint actionId) public view returns (address ownerAddress,bool initialized, uint duration, uint timeZero, bool isActive)
+    ]"#,
+);
 
-const WSS_URL: &str = "wss://eth-sepolia.g.alchemy.com/v2/573YG81S6_ZUn8IWf6IPz5YopOeanlaj";
-const TOKEN_DELEGATOR_CONTRACT_ADDRESS: &str="0x48745dF521aC6a2822043F73d92b4045C9109246";
+
+const WSS_URL: &str = "https://eth-sepolia.g.alchemy.com/v2/rOnL9TIb2mwbMDatQfBX-BJXUFH4Weml";
+const TOKEN_DELEGATOR_CONTRACT_ADDRESS: &str="0x5335959B18b8793d0C2aBA301da50c91267EbcD6";
+const TRANSFER_ACTION_CONTRACT_ADDRESS: &str="0xf402c8fd146C3b36D573df2E3f29507DF1683261";
+const PRIV_KEY:&str = "0x0b7239cc8c8b2626112cb5679db56cfcc73f6f2a0f8f24b81b84cc6eaf0bfb58";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // let provider = Provider::<Ws>::connect(WSS_URL).await?;
-    // let client = Arc::new(provider);
 
-    // let now = Utc::now();
-    // let timestamp = now.timestamp();
-
-    // println!("Current Unix timestamp: {}", timestamp);
-
-    // let latest_block = client.get_block_number().await?;
-
-    // let target_address: Address = TOKEN_DELEGATOR_CONTRACT_ADDRESS.parse()?;
-
-    // println!("{} block", latest_block);
-    // println!("{} address", target_address);
-
-    // let filter = Filter::new()
-    // .address(target_address)
-    // .event("ActionExecutionAttempted(uint256,string,uint256,address)")
-    // .from_block(0);
-    let provider = Provider::<Ws>::connect(WSS_URL).await?;
-    let client = Arc::new(provider);
+    let provider = Provider::<Http>::try_from(WSS_URL)?;
+    let wallet:LocalWallet = PRIV_KEY.parse::<LocalWallet>()?.with_chain_id(Chain::Sepolia);
+    let client = Arc::new(SignerMiddleware::new(provider.clone(), wallet.clone()));
     let address: Address = TOKEN_DELEGATOR_CONTRACT_ADDRESS.parse()?;
-    let contract = MyContract::new(address, client.clone());
 
     let latest_block = client.get_block_number().await?;
     println!("{} block", latest_block);
 
-    listen_specific_events(&contract, &latest_block).await?;
+    listen_specific_events(&client, &address).await?;
+
 
     Ok(()) 
 }
 
 
-async fn listen_specific_events(contract: &MyContract<Provider<Ws>>,latest_block:&U64) -> Result<()> {
-    
-    let events = contract.event::<ActionExecutionAttemptedFilter>().from_block(latest_block);
-    let mut stream = events.stream().await?.take(1);
+async fn listen_specific_events(client:&Client,contract_addr:&H160) -> Result<()> {
 
+    let contract = MyContract::new(contract_addr.clone(), Arc::new(client.clone()));
+
+    let transferAddress:Address = TRANSFER_ACTION_CONTRACT_ADDRESS.parse()?;
+    let transferContract = TransferAction::new(transferAddress, Arc::new(client.clone()));
+
+    let events = contract.event::<ActionExecutionAttemptedFilter>().from_block(6177567);
+    let mut stream = events.stream().await?.take(1);
+    
     while let Some(Ok(event)) = stream.next().await {
         println!("SomeEvent event: {event:?}");
-        println!("SomeEvent event: {:?}", event.message);
+        let mut time_zero = event.time_zero.as_u64();
         let action_id = event.action_id;
-        match contract.execute_action(action_id).send().await {
-            Ok(tx) => println!("Transaction sent: {:?}", tx),
-            Err(e) => println!("Error sending transaction: {:?}", e),
-        }
+        loop{
+            let now = SystemTime::now();
+            let target_time = UNIX_EPOCH + Duration::from_secs(time_zero+5);
+            let duration_until_target = target_time.duration_since(now)?;
+            let target_instant = Instant::now() + duration_until_target;
+            println!("im before sleep");
+            sleep_until(target_instant).await;
+            
+            let tx = contract.execute_action(U256::from(action_id).to_owned());
+            println!("tx {:?}",tx);
+            let pending_tx = tx.send().await?;
+            println!("pending_tx {:?}",pending_tx);
+            let mined_tx = pending_tx.await?;
+            println!("mined_tx {:?}",mined_tx);
+            println!("Transaction Receipt: {}",serde_json::to_string(&mined_tx)?);
+            let action = transferContract.get_action_by_id(action_id.clone()).call().await?;
+            let (address, initialized, duration,time_zero_from_block, is_active) = action;
+            println!("im after sleep");
+            if !is_active{
+                break;
+            }
+            println!("new timestamp {:?}",time_zero_from_block);
+            time_zero = time_zero_from_block.as_u64();
+         }
     }
 
     Ok(())
---
+++ minimal_example/src/main.rs
@@ -0,0 +1,74 @@
+use ethers::{
+    types::U256,
+    contract::abigen,
+    core::types::{Address, Filter},
+    providers::{Provider, Ws},
+};
+use ethers::prelude::*;
+use eyre::Result;
+use resolver::print;
+use serde_json::Number;
+use std::{sync::Arc, time::{SystemTime, UNIX_EPOCH}};
+use tokio::time::{sleep_until, Instant, Duration};
+use chrono::{Utc};
+use hex_literal::hex;
+use std::str;
+
+type Client = SignerMiddleware<Provider<Http>, 
+Wallet<k256::ecdsa::SigningKey>>;
+
+abigen!(
+    TokenManager,
+    r#"[
+    function setBalance(address user, uint amount) public
+    function getBalance(address user) public view returns (uint)
+    function transfer(address from, address to, uint amount) public returns (bool)
+    ]"#,
+);
+
+abigen!(
+    ActionExecutor,
+    r#"[
+    function executeTransfer(address from, address to, uint amount) public returns (bool)
+    ]"#,
+);
+
+
+const WSS_URL: &str = "https://eth-sepolia.g.alchemy.com/v2/rOnL9TIb2mwbMDatQfBX-BJXUFH4Weml";
+const TOKEN_MANAGER_CONTRACT_ADDRESS: &str="0x272447E0FD895158Ac2B1897e2EC9ccf1A6a8b5D";
+const ACTION_EXECUTOR_CONTRACT_ADDRESS: &str="0x0C5c47ba5636f990E6c3b7e66CB52Dc611719EbB";
+const PRIV_KEY:&str = "0x0b7239cc8c8b2626112cb5679db56cfcc73f6f2a0f8f24b81b84cc6eaf0bfb58";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let provider = Provider::<Http>::try_from(WSS_URL)?;
+    let wallet:LocalWallet = PRIV_KEY.parse::<LocalWallet>()?.with_chain_id(Chain::Sepolia);
+    let client = SignerMiddleware::new(provider.clone(), wallet.clone());
+    let token_manager_address: Address = TOKEN_MANAGER_CONTRACT_ADDRESS.parse()?;
+    let executor_address: Address = ACTION_EXECUTOR_CONTRACT_ADDRESS.parse()?;
+
+    // let contract = MyContract::new(contract_addr.clone(), Arc::new(client.clone()));
+    let token_manager = TokenManager::new(token_manager_address.clone(),Arc::new(client.clone()));
+    let action_executor = ActionExecutor::new(executor_address.clone(),Arc::new(client.clone()));
+
+    let tx1 = token_manager.set_balance("0x8C33f3Cd815e4C0624E53FadCf0fC21e19125bdD".parse()?,U256::from(1000)).send().await?.await?;
+    println!("tx obj {:?}",tx1);
+    let balance = token_manager.get_balance("0x8C33f3Cd815e4C0624E53FadCf0fC21e19125bdD".parse()?).call().await?;
+    println!("balance of user1 {:?}",balance);
+    let tx2 = token_manager.set_balance("0xF7829a3addC8BAD498fAa73370889c023D1295C5".parse()?,U256::from(6969)).send().await?.await?;
+    println!("tx obj {:?}",tx2);
+    let balance2 = token_manager.get_balance("0xF7829a3addC8BAD498fAa73370889c023D1295C5".parse()?).call().await?;
+    println!("balance of user2 {:?}",balance2);
+
+    let execute_transfer_tx = action_executor.execute_transfer("0x8C33f3Cd815e4C0624E53FadCf0fC21e19125bdD".parse()?, "0xF7829a3addC8BAD498fAa73370889c023D1295C5".parse()?, U256::from(20)).send().await?.await?;
+    println!("executied transfer {:?}",execute_transfer_tx);
+
+    let balance = token_manager.get_balance("0x8C33f3Cd815e4C0624E53FadCf0fC21e19125bdD".parse()?).call().await?;
+    println!("balance of user1 {:?}",balance);
+    let balance2 = token_manager.get_balance("0xF7829a3addC8BAD498fAa73370889c023D1295C5".parse()?).call().await?;
+    println!("balance of user2 {:?}",balance2);
+
+    Ok(()) 
+}
+
